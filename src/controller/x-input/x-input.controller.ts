446fa9176cf09d58c44651a4ae72849b54f40ab9
Zoya <zoya@eminds.ai>
2024-01-05 01:31:34 +0530

    Added x input for to on init response and done formatting

--- src/controller/x-input/x-input.controller.ts
+++ src/controller/x-input/x-input.controller.ts
@@ -1,32 +1,44 @@
-import { Request, Response } from 'express';
-import fs from 'fs';
-import ejs from 'ejs';
-import { controller, httpPost, interfaces, requestBody, response } from 'inversify-express-utils';
+import { Request, Response } from "express";
+import fs from "fs";
+import ejs from "ejs";
+import {
+  controller,
+  httpPost,
+  interfaces,
+  requestBody,
+  response,
+} from "inversify-express-utils";
 import httpStatus from "http-status";
-import path from 'path';
+import path from "path";
 import appRootPath from "app-root-path";
-import Joi from 'joi'
 import { convertToJoiSchema } from "../../util/validate.util";
 
-
-
-@controller('/x-input')
+@controller("/x-input")
 export class XInputController implements interfaces.Controller {
   constructor() { }
 
-  @httpPost('/form')
-  public async getForm(@requestBody() body: any, @response() res: Response): Promise<void> {
+  @httpPost("/form")
+  public async getForm(
+    @requestBody() body: any,
+    @response() res: Response
+  ): Promise<void> {
     try {
       const { action } = body.context || {};
       if (!action) {
         res.status(httpStatus.BAD_REQUEST).send();
         return;
       }
-      
-      const template = fs.readFileSync(path.join(appRootPath.toString(), `/src/template/x-input/${action}/${action}.ejs`), "utf8")
+
+      const template = fs.readFileSync(
+        path.join(
+          appRootPath.toString(),
+          `/src/template/x-input/${action}/${action}.ejs`
+        ),
+        "utf8"
+      );
       const compiledTemplate = ejs.compile(template);
       const renderedHTML = compiledTemplate();
-      res.setHeader('Content-Type', 'text/html');
+      res.setHeader("Content-Type", "text/html");
       res.write(renderedHTML);
       res.status(httpStatus.OK).send();
     } catch (error) {
@@ -34,29 +46,32 @@ export class XInputController implements interfaces.Controller {
       return undefined;
     }
   }
-  @httpPost('/submit')
-  public async submitForm(@requestBody() body: any, @response() res: Response): Promise<void> {
+  @httpPost("/submit")
+  public async submitForm(
+    @requestBody() body: any,
+    @response() res: Response
+  ): Promise<void> {
     try {
-      interface Payload {
-        name?: string;
-        age?: number;
-        [key: string]: unknown; 
-      }
       const { action } = body.context || {};
       if (!action) {
         res.status(httpStatus.BAD_REQUEST).send();
         return;
       }
-      const data=body.message
-      const validationRules=fs.readFileSync(path.join(appRootPath.toString(),`/src/template/x-input/on_init/validation_rules.json`),"utf-8")
+      const data = body.message;
+      const validationRules = fs.readFileSync(
+        path.join(
+          appRootPath.toString(),
+          `/src/template/x-input/${action}/rules.json`
+        ),
+        "utf-8"
+      );
       const schema = convertToJoiSchema(JSON.parse(validationRules));
       const { error, value } = schema.validate(data);
-      if(error != undefined){
-        res.status(httpStatus.OK).send({error: error.details[0].message});
+      if (error != undefined) {
+        res.status(httpStatus.OK).send({ error: error.details[0].message });
         return;
       }
-   
- 
+
       res.status(httpStatus.OK).send(value);
     } catch (error) {
       res.status(httpStatus.SERVICE_UNAVAILABLE).send();
