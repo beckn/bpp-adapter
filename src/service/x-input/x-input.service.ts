73a9226f99ad11d68c5d22eb1cc8d1b1803a7c81
Zoya <zoya@eminds.ai>
2024-01-10 15:00:13 +0530

    Xinput details saving to Strapi BPP

--- src/service/x-input/x-input.service.ts
+++ src/service/x-input/x-input.service.ts
@@ -3,16 +3,23 @@ import { makeGraphQLRequest } from "../../util/api";
 
 @injectable()
 export class XInputService {
-  constructor() { }
-
+  constructor() {}
   async xInput(filter: any) {
     const currentDate = new Date();
     const isoString = currentDate.toISOString();
 
+    const xInputData = Object.entries(filter).map((ele) => {
+      if (typeof ele[1] === "string") {
+        return `${ele[0]}:"${ele[1]}"`;
+      } else {
+        return `${ele[0]}:${ele[1]}`;
+      }
+    });
     const query = `mutation {
     createInputDetail(
       data: {
-       form_data: ${filter}
+       form_data: {${xInputData}},
+       form_id:"${filter.form_id}"
       publishedAt:"${isoString}"
       }
     ) {
@@ -27,7 +34,5 @@ export class XInputService {
       submissionId: response.data.createInputDetail.data.id,
     };
     return output;
-  };
+  }
 }
-
-
